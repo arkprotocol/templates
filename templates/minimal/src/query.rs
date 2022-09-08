3be304bedcbc1d1b275091b356b5b17e43982612
Yan Maya <r3mix.il@gmail.com>
2022-09-08 17:56:57 +0300

    ...

--- templates/minimal/src/query.rs
+++ templates/minimal/src/query.rs
@@ -2,7 +2,7 @@
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, Env, StdResult};
 
-use crate::msg::{QueryMsg};
+use crate::msg::QueryMsg;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
