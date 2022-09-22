51be6a8313d9782e406e71abd5db591ed5203282
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2022-09-22 22:41:41 +0200

    #18 - cleanup controller and move to tests, since they are test specific

--- tests/src/controller.ts
+++ tests/src/controller.ts
@@ -1,35 +1,6 @@
 import { CosmWasmSigner } from "@confio/relayer";
 import { ExecuteResult } from "@cosmjs/cosmwasm-stargate";
 
-export interface ibcPingResponse {
-  result: string;
-}
-
-export interface Connections {
-  connections: string[];
-}
-
-export interface Counter {
-  count: number;
-}
-
-export function showConnections(
-  cosmwasm: CosmWasmSigner,
-  contractAddr: string
-): Promise<Connections> {
-  const query = { get_connections: {} };
-  return cosmwasm.sign.queryContractSmart(contractAddr, query);
-}
-
-export function showCounter(
-  cosmwasm: CosmWasmSigner,
-  contractAddr: string,
-  channel: string
-): Promise<Counter> {
-  const query = { get_counter: { channel } };
-  return cosmwasm.sign.queryContractSmart(contractAddr, query);
-}
-
 export function executeContract(
   client: CosmWasmSigner,
   contractAddr: string,
